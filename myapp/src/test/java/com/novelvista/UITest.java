401fbd7b58bf930eaab009ab4e2c226444e09c3f
vebhav24 <166806636+vebhav24@users.noreply.github.com>
2025-10-07 18:42:32 +0530

    Update UITest.java

--- myapp/src/test/java/com/novelvista/UITest.java
+++ myapp/src/test/java/com/novelvista/UITest.java
@@ -1,30 +1,33 @@
 package com.novelvista;
 
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.*;
 
 public class UITest {
-    private WebDriver driver;
-    private final String baseUrl = System.getProperty("baseUrl", "http://localhost:8080/myapp");
+    WebDriver driver;
 
     @BeforeEach
-    void setup() {
+    public void setup() {
+        // Automatically download the correct ChromeDriver version
         WebDriverManager.chromedriver().setup();
-        ChromeOptions opts = new ChromeOptions();
-        opts.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
-        driver = new ChromeDriver(opts);
-    }
 
-    @AfterEach
-    void teardown() {
-        if (driver != null) driver.quit();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
-    void testTitle() {
-        driver.get(baseUrl);
-        Assertions.assertTrue(driver.getTitle().contains("MyApp"));
+    public void testTitle() {
+        driver.get("http://localhost:8080/myapp");
+        String title = driver.getTitle();
+        Assertions.assertTrue(title.contains("MyApp"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
